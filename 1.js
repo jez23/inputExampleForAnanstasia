de9d4f190db3a842d5d049c5ac849a2dca2d312e
Jeremy Blackmore <jeremyblackmore@Jeremys-iMac.local>
2021-11-24 23:19:13 +0000

    adding imports

--- 1.js
+++ 1.js
@@ -1,3 +1,5 @@
+import { addEventListeners } from './2.js';
+
 const createHtML = () => {
 
     let priceDisplay = document.createElement('div');
@@ -14,43 +16,6 @@ const createHtML = () => {
     document.body.appendChild(errorMessage);
 
     addEventListeners();
-}
-
-const addEventListeners = () => {
-
-    const priceInput = document.querySelector('#priceInput');
-    const priceDisplay = document.querySelector('#priceDisplay');
-    const errorMessage = document.querySelector('#errorMessage');
-    const priceWrap = document.querySelector('#price-wrap');
-
-    priceInput.addEventListener('change', (e) => {
-        if (e.target.value < 0) {
-            priceWrap.style.border = '1px solid red';
-            errorMessage.innerHTML = `Please enter correct price`;
-            priceDisplay.innerHTML = "";
-        } else {
-            priceWrap.style.border = '1px solid black';
-            priceDisplay.innerHTML = `<span id="currentPrice">Current price: ${e.target.value}</span><button id="removePrice">X</button>`;
-            errorMessage.innerHTML = "";
-        }
-    })
-
-    priceDisplay.addEventListener('click', (e) => {
-        if (e.target.id === "removePrice") {
-            priceWrap.value = "";
-            priceDisplay.innerHTML = "";
-        }
-    })
-
-    priceWrap.addEventListener('input', (e) => {
-        if (e.target.value < 0) {
-            priceWrap.style.color = 'red';
-            priceWrap.style.outlineColor = "red";
-        } else {
-            priceWrap.style.color = 'green';
-            priceWrap.style.outlineColor = "green";
-        }
-    })
 }
 
 createHtML();
